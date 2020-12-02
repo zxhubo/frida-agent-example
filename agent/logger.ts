3877f288aaecfd37d3393e96243cb425fe5b92be
zxhubo <zxhubo@126.com>
2020-12-02 20:07:34 +0800

    Update logger.ts

--- agent/logger.ts
+++ agent/logger.ts
@@ -1,3 +1,8 @@
 export function log(message: string): void {
     console.log(message);
 }
+
+export function printStackTrace(params:void) {
+    var logClz = Java.use("android.util.Log");
+    console.log(logClz.getStackTraceString(Java.use("java.lang.Exception").$new()));
+}
